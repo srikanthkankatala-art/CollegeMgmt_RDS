fb44265de78006061b4398d39e0095c37dd4a44f
srikanthkankatala-art <srikanthkankatala@gmail.com>
2025-08-13 15:08:02 +0530

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'cmtsdb.cr46q8o6wzab.ap-south-1.rds.amazonaws.com',
-	'username' => 'admin',
-	'password' => 'admin123',
+	'hostname' => 'srikanthsn.mysql.database.azure.com',
+	'username' => 'SrikanthAzureAdmin',
+	'password' => 'Verity@123',
 	'database' => 'college_mgmt',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
